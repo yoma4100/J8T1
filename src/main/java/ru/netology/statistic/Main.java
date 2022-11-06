54904ae8401dc9cddc0fe38014a239c2714844c2
Dmitry Bychkov <yom.4100@gmail.com>
2022-11-06 12:30:15 +0300

    add some code

--- src/main/java/ru/netology/statistic/Main.java
+++ src/main/java/ru/netology/statistic/Main.java
@@ -1,7 +1,5 @@
 package ru.netology.statistic;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String[] args) {}
 }
